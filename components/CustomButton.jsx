deaf86d2cc9e611c525f175c455ba463bf85e18e
Raveen <raveenrv92@gmail.com>
2024-10-10 17:31:39 +0530

    Designed Main Page

---
+++ components/CustomButton.jsx
@@ -0,0 +1,27 @@
+import { TouchableOpacity, Text } from "react-native";
+import React from "react";
+
+const CustomButton = ({
+  title,
+  handlePress,
+  containerStyles,
+  textStyles,
+  isLoading,
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-secondary rounded-xl justify-center items-center min-h-[62px] ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      }`}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
