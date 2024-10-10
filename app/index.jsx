6e05d832970a9a0134ddfc3b0dab9f110a69b11c
Raveen <raveenrv92@gmail.com>
2024-10-10 16:00:39 +0530

    Complete font setup

--- app/index.jsx
+++ app/index.jsx
@@ -4,19 +4,10 @@ import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-psemibold">New Text</Text>
       <StatusBar style="auto" />
       <Link href={"/profile"}>Go to profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
