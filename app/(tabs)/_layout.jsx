faefee66579d690898f90f846ccf2e3eb6ec99c7
Raveen <raveenrv92@gmail.com>
2024-10-10 16:14:17 +0530

    Created File Structure

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const TabsLaayout = () => {
+  return (
+    <View>
+      <Text>TabsLaayout</Text>
+    </View>
+  );
+};
+
+export default TabsLaayout;
